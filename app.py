8c51829fb1ca99cd8a6f895151dacc5685070a7a
FloLad38 <florian.ladreyt@le-campus-numerique.fr>
2025-04-09 15:56:01 +0200

    création de la page de photo dynamique

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +11,19 @@ def page_acceuil():
 @app.route("/gallery")
 def page_gallery():
     return render_template("gallery.html")
+
+@app.route("/gallery_dynamique")
+def page_gallery_dynamique():
+    chemin_images = os.path.join(app.static_folder, "Images")
+    
+    extensions_autorisees = ('.jpg', '.jpeg', '.png', '.gif')
+    liste_images = [
+    f for f in os.listdir(chemin_images)
+    if f.lower().endswith(extensions_autorisees)
+    ]
+    images_urls = [url_for('static', filename=f'Images/{img}') for img in liste_images]
+
+    return render_template('gallery_dynamique.html', images=images_urls)
 
 
 if __name__ == "__main__":
